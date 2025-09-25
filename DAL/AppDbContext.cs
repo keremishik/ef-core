f8057a95c1c2336ed3d654d3cac49c79945d246c
Kerem Isik <kerem.ishik@gmail.com>
2025-09-25 13:37:27 +0300

    feat(db): add AppDbContext with Products DbSet and SQL Server config
    Implement the database context for EF Core with initial Product entity configuration and SQL Server connection setup. This establishes the foundation for database operations in the application.

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningEFCore.DatabaseFirst.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        // DbSet properties represent database tables and allow querying/saving of entity instances
+        // My Product Table Entity
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=ISTNB0630\\SQLEXPRESS;Initial Catalog=LearningEFCore.DatabaseFirstDB;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
