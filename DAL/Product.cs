28025e395a358d9f81f55b0e1374020f34269838
Kerem Isik <kerem.ishik@gmail.com>
2025-09-25 13:32:06 +0300

    feat(DAL): add Product entity class for database mapping
    Introduce the Product entity class to represent the product table in the database. This class includes Id, Name, and Price properties which will be mapped to corresponding database columns.

---
+++ DAL/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningEFCore.DatabaseFirst.DAL
+{
+    // Entity classes are special classes that represent database tables
+    // Unlike regular classes:
+    // - Entities are mapped to database tables
+    // - Their properties correspond to table columns
+    // - They can have relationships with other entities
+    // - They are tracked by Entity Framework for change detection
+    // - They often include data annotations and configurations
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
